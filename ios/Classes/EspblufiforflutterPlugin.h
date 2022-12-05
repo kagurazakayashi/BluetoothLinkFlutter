792063ea5e6880dde160a0e3bfab3ecd47dd7349
神楽坂雅詩 <cyscxchope@gmail.com>
2022-12-05 16:58:22 +0800

    语言降级

---
+++ ios/Classes/EspblufiforflutterPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface EspblufiforflutterPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/EspblufiforflutterPlugin.m
@@ -0,0 +1,20 @@
+#import "EspblufiforflutterPlugin.h"
+
+@implementation EspblufiforflutterPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"espblufiforflutter"
+            binaryMessenger:[registrar messenger]];
+  EspblufiforflutterPlugin* instance = [[EspblufiforflutterPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
